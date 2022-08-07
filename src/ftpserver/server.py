849e254de8509d7dc91a5cc24bd3d091b36c43f6
Kaialogen <kaialogen@protonmail.com>
2022-08-07 18:48:32 +0100

    new_compact

--- src/ftpserver/server.py
+++ src/ftpserver/server.py
@@ -1,54 +1,35 @@
-import socket
-import sys
-import time
 import os
-import struct
 
-print("\nWelcome to the FTP server.\n\nTo get started, connect a client")
-TCP_IP = "127.0.0.1"
-TCP_PORT = 21
-BUFFER_SIZE = 4096
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((TCP_IP, TCP_PORT))
-s.listen(1)
-conn, addr = s.accept()
-
-print("\nConnected to by address: {}".format(addr))
-
-
-def list_files():
-    print("Listing files...")
-    listing = os.listdir(os.getcwd())
-    conn.send(struct.pack("i", len(listing)))
-    total_directory_size = 0
-    for i in listing:
-        conn.send(struct.pack("i", sys.getsizeof(i)))
-        conn.send(i)
-        conn.send(struct.pack("i", os.path.getsize(i)))
-        total_directory_size += os.path.getsize(i)
-        conn.recv(BUFFER_SIZE)
-    conn.send(struct.pack("i", total_directory_size))
-    conn.recv(BUFFER_SIZE)
-    print("Successfully sent file listing")
-    return
+from pyftpdlib.authorizers import DummyAuthorizer
+from pyftpdlib.handlers import FTPHandler
+from pyftpdlib.servers import FTPServer
 
 def main():
-    while True:
-        data = conn.recv(BUFFER_SIZE)
-        data = data.decode(encoding='utf_8', errors='strict')
-        if data == "LIST":
-            print("LIST")
-            list_files()
-        elif data == "QUIT":
-            print("QUIT")
-            quit()
-        data = None
+    # Instantiate a dummy authorizer for managing 'virtual' users
+    authorizer = DummyAuthorizer()
 
-def quit():
-    conn.send("1".encode())
-    conn.close()
-    s.close()
-    os.execl(sys.executable, sys.executable, *sys.argv)
+    # Define a new user having full r/w permissions and a read-only
+    # anonymous user
+    authorizer.add_user('user', '12345', '.', perm='elradfmwMT')
+    authorizer.add_anonymous(os.getcwd())
+
+    # Instantiate FTP handler class
+    handler = FTPHandler
+    handler.authorizer = authorizer
+
+    # Define a customized banner (string returned when client connects)
+    handler.banner = "pyftpdlib based ftpd ready."
+
+    # Instantiate FTP server class and listen on 127.0.0.1:2121
+    address = ('127.0.0.1', 2121)
+    server = FTPServer(address, handler)
+
+    # set a limit for connections
+    server.max_cons = 256
+    server.max_cons_per_ip = 5
+
+    # start ftp server
+    server.serve_forever()
 
 if __name__ == '__main__':
     main()
