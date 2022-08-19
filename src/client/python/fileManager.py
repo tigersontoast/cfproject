d45ec54c6e7ce7a65b94c7202c15a332696182b7
Jacob Stockwell <ed20jes@leeds.ac.uk>
2022-08-19 14:36:37 +0100

    Added fileManager stubs

---
+++ src/client/python/fileManager.py
@@ -0,0 +1,20 @@
+####TO DO####
+
+class fileManager():
+    def __init__(self, dataManager):
+        pass
+
+    def fetch(fileName):
+        pass
+
+    def read():
+        pass
+
+    def write():
+        pass
+
+    def move(fromString, toString):
+        pass
+
+    def log(message):
+        pass
---
+++ src/client/python/system.py
@@ -0,0 +1,10 @@
+from fileManager import fileManager
+from dataManager import dataManager
+import art
+
+class system():
+    def __init__(self):
+        
+        self.dataManager = ()
+        self.fileManager = fileManager()
+
